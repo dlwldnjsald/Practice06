41273e8620d2b829f14aaa231d4ad9d21fed80c2
dlwldnjsald <dlwldnjsald@gmail.com>
2024-03-27 18:35:28 +0900

    commit push 0327 18:35

--- src/com/javaex/ex01/Customer.java
+++ src/com/javaex/ex01/Customer.java
@@ -1,6 +1,61 @@
 package com.javaex.ex01;
 
-public class Customer extends Person{ ////
+public class Customer extends Person {		//부모 person클래스 상속받기
+	
+	//필드 만들기-----------------------
+		private int cNo;
+		private int point;
+		
+	//--------------------------------
+		
+		//Person클래스에서 기본 생성자인 
+	    //public Person() { }를 선언했으므로
+		
+		public Customer(String name, String hp, int cNo, int point) {
+			super(name, hp);
+			this.cNo = cNo;
+			this.point = point;
+			
+			System.out.println( " Customer(3 parameter))");
+		} //로 작성해주고
+	
+	//----------------------------------
+		//getter,setter
+		public Customer(int cNo, int point) {
+			super("Unknown", 0);
+			this.cNo = cNo;
+			this.point = point;
+			
+			System.out.println( " stuent (1 parameter))");// 
+		}
+		//-------------------------------
+		public int getcNo() {
+			return cN0;
+		}
+		
+		public void setcNo() {
+			this.cNo = cNo;
+			
+		}
+		
+		public int getpoint() {
+			return point;
+		}
+		
+		public void setpoint() {
+			this.point = point;
+			
+		}
+		//------------------------------------
+		@Override
+		public void showInfo() {
+			super.showInfo();
+			System.out.println("이름:" + name + ", #hp:" + hp + " #고객번호:" + cNo + " #포인트점수:" + point);	
+		}
+				
+		
+		
+		
 	
 
 	
--- src/com/javaex/ex01/Person.java
+++ src/com/javaex/ex01/Person.java
@@ -1,7 +1,55 @@
 package com.javaex.ex01;
 
 public class Person {
+	//person이 상위클래스 부모클래스, customer가 하위클래스 자식클래스로 설정됨
 	
+	private String name;
+	private String hp;      //필드에 name,hp 2개 선언;
 	
+	//----------------------------------------------
 	
+	//생성자// 
+
+	public Person() {
+		
+    }                       //기본 생성자 선언
+	
+	public Person(String name, String hp) {
+		this.name = name;
+		this.hp = hp;
+			
+	}								//생성자 name,hp
+	//외부로 받는 name데이터를 이 위의 name변수에 할당해준다
+	//외부로 받는 hp데이터를 이 위의 hp변수에 핼당해준다
+	
+	//------------------------------------------------
+	
+	//getter,setter//
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) { //return안하니까 void
+		this.name = name;				//외부name값 할당
+	}
+	
+	//--------------------------------
+	public String gethp() {
+		return hp;
+	}
+	public void sethp(String hp) {
+		this.hp = hp;
+	}
+	
+	//--------------------------------------------------
+	
+	//showInfo()영역만들기//
+	
+	public void showInfo() {
+		System.out.println("이름:" + name + ", #hp:" + hp);
+		
+	}
+	
+	//--------------------------------------------------
+		
 }
